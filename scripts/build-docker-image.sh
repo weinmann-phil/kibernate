592d2355125d4ea6aaaef6174761d0ea01e14f7a
Michael Werner <michaelwerner42@gmail.com>
2023-03-26 09:32:22 -0700

    fix(feat/ci-build-publish): move minikube docker-env to allow for universally usable docker build script

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -3,5 +3,4 @@
 set -eo pipefail
 
 cd "$(dirname "$0")"/..
-eval "$(minikube -p kibernate-test docker-env)"
 docker build -f build/package/docker/Dockerfile -t kibernate:latest .
--- scripts/prepare-testing-env.sh
+++ scripts/prepare-testing-env.sh
@@ -4,3 +4,4 @@ set -eo pipefail
 
 minikube start --driver=docker --kubernetes-version=v1.24.10 -p kibernate-test
 minikube profile kibernate-test
+eval "$(minikube docker-env)"
