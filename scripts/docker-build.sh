d14941cb8f0c676db05052a4b9d1b05c460475a8
Michael Werner <michaelwerner42@gmail.com>
2023-03-26 11:18:37 -0700

    fix(feat/ci-build-publish): move minikube docker-env into right location

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -8,4 +8,10 @@ if [[ -z "$1" ]]; then
 fi
 
 cd "$(dirname "$0")"/..
+
+if [[ -n "$(command -v minikube)" ]] && minikube profile list | grep -q kibernate-test; then
+  echo "minikube profile kibernate-test exists - assuming it is build environment"
+  eval "$(minikube -p kibernate-test docker-env)"
+fi
+
 docker buildx build --platform="$1" -f build/package/docker/Dockerfile -t kibernate:latest .
--- scripts/prepare-testing-env.sh
+++ scripts/prepare-testing-env.sh
@@ -4,4 +4,3 @@ set -eo pipefail
 
 minikube start --driver=docker --kubernetes-version=v1.24.10 -p kibernate-test
 minikube profile kibernate-test
-eval "$(minikube docker-env)"
