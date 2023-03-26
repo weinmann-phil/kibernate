6f995307f213c3f510739026b457a504e45bc643
Michael Werner <michaelwerner42@gmail.com>
2023-03-26 12:21:57 -0700

    fix(fix/ci-multi-platform-build): add --output type=registry to docker buildx build commandline

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -9,7 +9,7 @@ fi
 
 cd "$(dirname "$0")"/..
 
-docker buildx build --platform="$1" -f build/package/docker/Dockerfile -t kibernate:latest .
+docker buildx build --output type=registry --platform="$1" -f build/package/docker/Dockerfile -t kibernate:latest .
 
 if [[ -n "$(command -v minikube)" ]] && minikube profile list | grep -q kibernate-test; then
   echo "minikube profile kibernate-test exists - importing image into minikube"
