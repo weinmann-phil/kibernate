9b72a6a02bdbb5299064fa5581b9652388199973
Michael Werner <michaelwerner42@gmail.com>
2023-03-25 22:38:32 -0700

    chore(deployments/helm): move kibernate settings from args to separate "kibernate" map

--- scripts/install-helm-chart.sh
+++ scripts/install-helm-chart.sh
@@ -10,9 +10,8 @@ helm upgrade --install \
   ./deployments/helm/kibernate \
   --set image.tag=latest \
   --set image.pullPolicy=Never \
-  --set "args[0]=-service=testtarget" \
-  --set "args[1]=-servicePort=8080" \
-  --set "args[2]=-deployment=testtarget" \
-  --set "args[3]=-namespace=default" \
-  --set "args[4]=-defaultWaitType=connect" \
-  --set "args[5]=-idleTimeoutSecs=60"
+  --set kibernate.service=testtarget \
+  --set kibernate.namespace=default \
+  --set kibernate.defaultWaitType=connect \
+  --set kibernate.idleTimeoutSecs=60 \
+  --set kibernate.deployment=testtarget
