5bd08b02981712b92ae5a4df0c3287fd7c2493eb
Michael Werner <michaelwerner42@gmail.com>
2023-04-02 13:11:19 -0700

    feat(extend-supported-platforms): add the supported platforms of k8s

--- scripts/docker-buildx-push-ghcr.sh
+++ scripts/docker-buildx-push-ghcr.sh
@@ -13,4 +13,4 @@ for tag in "$@"; do
   tags="$tags -t ghcr.io/kibernate/kibernate:$tag"
 done
 
-docker buildx build --platform linux/amd64,linux/arm64 $tags --push -f build/package/docker/Dockerfile .
+docker buildx build --platform linux/amd64,linux/386,linux/arm/v7,linux/arm64,linux/s390x,linux/ppc64le $tags --push -f build/package/docker/Dockerfile .
