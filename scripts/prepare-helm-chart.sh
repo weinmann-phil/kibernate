a92c8d328c895b2320335d943699ca484772c3a3
Michael Werner <michaelwerner42@gmail.com>
2023-04-02 14:01:08 -0700

    fix(feat/helm-test-fallback-to-main-branch): fix logic error

--- scripts/prepare-helm-chart.sh
+++ scripts/prepare-helm-chart.sh
@@ -9,5 +9,8 @@ rm -fr kibernate-helm
 if [ "$BRANCH_NAME" != "main" ] && ! git clone -b $BRANCH_NAME --single-branch --depth 1 https://github.com/kibernate/kibernate-helm.git; then
   echo "Branch $BRANCH_NAME does not exist in kibernate-helm, falling back to main"
   git clone -b main --single-branch --depth 1 https://github.com/kibernate/kibernate-helm.git
+else
+  echo "Using branch $BRANCH_NAME in kibernate-helm"
+  git clone -b $BRANCH_NAME --single-branch --depth 1 https://github.com/kibernate/kibernate-helm.git
 fi
 rm -fr kibernate-helm/.git
