2e55618e21fb79362acbd853af9bc3f09996af18
Michael Werner <michaelwerner42@gmail.com>
2023-04-02 13:53:34 -0700

    feat(helm-test-fallback-to-main-branch): if current branch does not exist in kibernate-helm repo, fall back to main

--- scripts/prepare-helm-chart.sh
+++ scripts/prepare-helm-chart.sh
@@ -6,5 +6,8 @@ cd "$(dirname "$0")"/..
 
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 rm -fr kibernate-helm
-git clone -b $BRANCH_NAME --single-branch --depth 1 https://github.com/kibernate/kibernate-helm.git
+if [ "$BRANCH_NAME" != "main" ] && ! git clone -b $BRANCH_NAME --single-branch --depth 1 https://github.com/kibernate/kibernate-helm.git; then
+  echo "Branch $BRANCH_NAME does not exist in kibernate-helm, falling back to main"
+  git clone -b main --single-branch --depth 1 https://github.com/kibernate/kibernate-helm.git
+fi
 rm -fr kibernate-helm/.git
